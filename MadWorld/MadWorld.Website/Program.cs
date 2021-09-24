cf91a6e656a6453605c0ba4362965b4bd9913d83
Oscar Veldman <oveldman@gmail.com>
2021-09-24 17:02:27 +0200

    Test application insight

--- MadWorld/MadWorld.Website/Program.cs
+++ MadWorld/MadWorld.Website/Program.cs
@@ -7,11 +7,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BlazorApplicationInsights;
 
 namespace MadWorld.Website
 {
     public class Program
     {
+        private static string InstrumentationKey;
+
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -19,7 +22,37 @@ namespace MadWorld.Website
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            SetApplicationSettings(builder.Configuration);
+
+            builder.Services.AddBlazorApplicationInsights(async applicationInsights =>
+            {
+                await applicationInsights.SetInstrumentationKey(InstrumentationKey);
+                await applicationInsights.LoadAppInsights();
+            });
+
+            //AddExternPackages(builder.Services);
+            AddMadWorldClassesToScoped(builder.Services);
+
             await builder.Build().RunAsync();
+        }
+
+        private static void AddExternPackages(IServiceCollection services)
+        {
+            services.AddBlazorApplicationInsights(async applicationInsights =>
+            {
+                await applicationInsights.SetInstrumentationKey(InstrumentationKey);
+                await applicationInsights.LoadAppInsights();
+            });
+        }
+
+        private static void AddMadWorldClassesToScoped(IServiceCollection services)
+        {
+
+        }
+
+        private static void SetApplicationSettings(WebAssemblyHostConfiguration configuration)
+        {
+            InstrumentationKey = configuration["Azure:InstrumentationKey"];
         }
     }
 }
