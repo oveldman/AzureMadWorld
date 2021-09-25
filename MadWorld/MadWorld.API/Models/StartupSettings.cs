df7eeef96871dd8ae68f49d8be25a0c218b4d72c
Oscar Veldman <oveldman@gmail.com>
2021-09-26 00:41:49 +0200

    Fix migration

--- MadWorld/MadWorld.API/Models/StartupSettings.cs
+++ MadWorld/MadWorld.API/Models/StartupSettings.cs
@@ -4,5 +4,6 @@ namespace MadWorld.API.Models
     public class StartupSettings
     {
         public bool ForgePostgresMigration { get; set; }
+        public bool ForgeMsSqlMigration { get; set; }
     }
 }
