18a9dc04c14c4d3754169c941e15df2c6b00ce0a
Oscar Veldman <oveldman@gmail.com>
2021-12-20 19:05:29 +0100

    refactor

--- MadWorld/MadWorld.Business/RustLibrary/Test/LearnLib.cs
+++ MadWorld/MadWorld.Business/RustLibrary/Test/LearnLib.cs
@@ -5,14 +5,16 @@ namespace MadWorld.Business.RustLibrary.Test
 {
     public class LearnLib
     {
+        private const string libraryName = "learn_lib";
+
         // Return data in method result
-        [DllImport("learn_lib")]
+        [DllImport(libraryName)]
         public static extern int plus(int x, int y);
 
-        [DllImport("learn_lib")]
+        [DllImport(libraryName)]
         public static extern TestStruct test_struct_plus(int x, int y);
 
-        [DllImport("learn_lib")]
+        [DllImport(libraryName)]
         public static extern StringStruct test_struct_string();
     }
 }
