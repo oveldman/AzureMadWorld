ab612e8004e2b5a3e1b65aa9ff96984940ede373
Oscar Veldman <oveldman@gmail.com>
2022-01-06 09:52:16 +0100

    Add unittest to mapper

---
+++ MadWorld/MadWorld.Tests/Business/Mapper/IpfsMapperManagerTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using MadWorld.Business.Mapper;
+using MadWorld.DataLayer.Database.Tables;
+using MadWorld.Shared.Models.Admin.IPFS;
+using MadWorld.Shared.Models.IPFS;
+
+namespace MadWorld.Tests.Business.Mapper
+{
+	public class IpfsMapperManagerTests
+	{
+		[Theory]
+		[AutoDomainData]
+		public void Translate_IpfsFile_IpfsDTO(
+			IpfsMapperManager ipfsMapperManager,
+			IpfsFile file
+			)
+        {
+			// No Test data
+
+			// No Setup
+
+			// Act
+			IpfsDTO dto = ipfsMapperManager.Translate<IpfsFile, IpfsDTO>(file);
+
+			// Assert
+			IpfsDTO expectedDTO = new()
+			{
+				FileType = file.ContentType,
+				Hash = file.Hash,
+				Name = file.Name,
+			};
+
+			expectedDTO.Should().BeEquivalentTo(dto);
+
+			// No Teardown
+		}
+
+		[Theory]
+		[AutoDomainData]
+		public void Translate_IpfsFile_IpfsAdminDTO(
+			IpfsMapperManager ipfsMapperManager,
+			IpfsFile file
+			)
+		{
+			// No Test data
+
+			// No Setup
+
+			// Act
+			IpfsAdminDTO dto = ipfsMapperManager.Translate<IpfsFile, IpfsAdminDTO>(file);
+
+			// Assert
+			IpfsAdminDTO expectedDTO = new()
+			{
+				ID = file.ID,
+				FileType = file.ContentType,
+				Hash = file.Hash,
+				Name = file.Name,
+			};
+
+			expectedDTO.Should().BeEquivalentTo(dto);
+
+			// No Teardown
+		}
+
+		[Theory]
+		[AutoDomainData]
+		public void Translate_IpfsAdminDTO_IpfsFile(
+			IpfsMapperManager ipfsMapperManager,
+			IpfsAdminDTO dto
+			)
+		{
+			// No Test data
+
+			// No Setup
+
+			// Act
+			IpfsFile file = ipfsMapperManager.Translate<IpfsAdminDTO, IpfsFile>(dto);
+
+			// Assert
+			IpfsFile expectedFile = new()
+			{
+				ID = dto.ID.Value,
+				ContentType = dto.FileType,
+				Hash = dto.Hash,
+				Name = dto.Name,
+			};
+
+			expectedFile.Should().BeEquivalentTo(file);
+
+			// No Teardown
+		}
+	}
+}
+
--- MadWorld/MadWorld.Tests/GlobalUsings.cs
+++ MadWorld/MadWorld.Tests/GlobalUsings.cs
@@ -1,6 +1,7 @@
 ﻿global using AutoFixture;
 global using AutoFixture.AutoMoq;
 global using AutoFixture.Xunit2;
+global using FluentAssertions;
 global using Moq;
 global using Xunit;
 
