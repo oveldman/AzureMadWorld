8d1626d500ce407ea733daa30925beab56f3614b
Oscar Veldman <oveldman@gmail.com>
2022-01-13 21:55:07 +0100

    Refactor shell script

--- Scripts/compile-js.sh
+++ Scripts/compile-js.sh
@@ -13,6 +13,27 @@ add_tools_to_scope() {
     fi
 }
 
+build_typescript() {
+    echo "Build typescript"
+    cd ../typescript
+    tsc --build
+}
+
+check_if_pipeline() {
+    if [ -z "$OSTYPE" ]; then
+        echo "Exit script because pipeline"
+        exit 0
+    fi
+}
+
+convert_all_js_files() {
+    echo "Start converting javascript files"
+    cd ../js
+    for js_folder in "${JS_FOLDERS[@]}"; do
+        convert_js_browser_compatible $js_folder
+    done
+}
+
 convert_js_browser_compatible() {
     FOLDER="$1/*"
 
@@ -31,35 +52,37 @@ convert_file() {
     fi
 }
 
-echo "Start the compile script"
-echo "Script executed from: ${PWD}"
+end_script() {
+    echo "Script finished"
+}
 
-if [ -z "$OSTYPE" ]; then
-    echo "Exit script because pipeline"
-    exit 0
-fi
+remove_old_javascript() {
+    cd $START_FOLDER
+
+    echo "Remove old javascript files"
+    for js_folder in "${JS_FOLDERS[@]}"; do
+        rm -r "${js_folder}/*" 
+    done
+}
+
+start_script() {
+    echo "Start the compile script"
+    echo "Script executed from: ${PWD}"
+}
+
+start_script
+check_if_pipeline
 
 START_FOLDER="../MadWorld/MadWorld.Website/wwwroot/js"
 SCRIPT_TYPE="$1"
+JS_FOLDERS=("base" "extern" "intern")
 
-if [ $SCRIPT_TYPE == "C#" ]; then
+if [ "$SCRIPT_TYPE" == "C#" ]; then
     START_FOLDER="../../../wwwroot/js"
     add_tools_to_scope
 fi
 
-cd $START_FOLDER
-echo "Remove old javascript files"
-rm -r base/*
-rm -r extern/*
-rm -r intern/*
-
-echo "Build typescript"
-cd ../typescript
-tsc --build
-
-echo "Start converting javascript files"
-cd ../js
-convert_js_browser_compatible base
-convert_js_browser_compatible extern
-convert_js_browser_compatible intern
-echo "Script finished"
+remove_old_javascript
+build_typescript
+convert_all_js_files
+end_script
