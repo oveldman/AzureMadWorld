bf551f92d4e0c7de7833fe981ec974eafdd4e31d
Oscar Veldman <oveldman@gmail.com>
2022-01-22 23:09:08 +0100

    Add mobile

--- MadWorld/MadWorld.Website/Extension/IServiceCollectionExtensions.cs
+++ MadWorld/MadWorld.Website/Extension/IServiceCollectionExtensions.cs
@@ -1,6 +1,19 @@
 ﻿using System;
+using MadWorld.Shared.DesignPattern;
 using MadWorld.Website.JavascriptManager;
 using MadWorld.Website.JavascriptManager.Interfaces;
+using MadWorld.Website.Manager;
+using MadWorld.Website.Manager.Interfaces;
+using MadWorld.Website.Models.Tools.Running;
+using MadWorld.Website.Services.Admin;
+using MadWorld.Website.Services.Admin.Interfaces;
+using MadWorld.Website.Services.Authorization;
+using MadWorld.Website.Services.Info;
+using MadWorld.Website.Services.Interfaces;
+using MadWorld.Website.Services.Support;
+using MadWorld.Website.Services.Tools;
+using MadWorld.Website.State;
+using MadWorld.Website.State.Interface;
 using Microsoft.JSInterop;
 
 namespace MadWorld.Website.Extension
@@ -9,7 +22,25 @@ namespace MadWorld.Website.Extension
 	{
 		public static IServiceCollection AddInternalClasses(this IServiceCollection services)
         {
-			return services;
+            //Design pattern
+            services.AddScoped<Iterator<RunRound>, RunRoundIterator>();
+
+            //Managers
+            services.AddScoped<IRunningManager, RunningManager>();
+            services.AddScoped<IAudioManager, AudioManager>();
+            services.AddScoped<ISmartlookManager, SmartlookManager>();
+
+            // Services
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IIpfsManagerService, IpfsManagerService>();
+            services.AddScoped<IIpfsService, IpfsService>();
+            services.AddScoped<IResumeService, ResumeService>();
+            services.AddScoped<ISecurityService, SecurityService>();
+            services.AddScoped<IUserManagerService, UserManagerService>();
+
+            // State
+            services.AddScoped<IAuthenticationHandler, AuthenticationHandler>();
+            return services;
         }
 	}
 }
