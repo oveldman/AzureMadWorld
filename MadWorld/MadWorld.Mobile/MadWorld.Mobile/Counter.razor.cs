2d386137d38c71ca9b847f990caf036b933a23ef
Oscar Veldman <oveldman@gmail.com>
2022-01-23 20:55:09 +0100

    Refactor mobile app

---
+++ MadWorld/MadWorld.Mobile/MadWorld.Mobile/Counter.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MadWorld.Mobile
+{
+	public partial class Counter
+	{
+        int count;
+
+        void IncrementCount()
+        {
+            count++;
+        }
+    }
+}
+
